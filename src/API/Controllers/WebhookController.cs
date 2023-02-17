1ddbc777832f5aa9362b781a78e84f6e9551d51e
Tim Randall <Tim@milyli.com>
2023-02-17 14:55:01 -0600

    feat: Add webhook step model and update webhooks to use it.

--- src/API/Controllers/WebhookController.cs
+++ src/API/Controllers/WebhookController.cs
@@ -1,4 +1,5 @@
 ﻿using DelegateLearningDocs.Hangfire.QueueManagers;
+using DelegateLearningDocs.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
 
@@ -19,9 +20,9 @@ namespace DelegateLearningDocs.Controllers
 
         [HttpPost]
         [Route("hangfire/slackmessage")]
-        public IActionResult HangfireSlackMessageWebhook()
+        public IActionResult HangfireSlackMessageWebhook(WebhookStep step)
         {
-            var message = "Hey there! I'm a background processed message using Hangfire! Robots are cool! Beep boop beep :robot_face:";
+            var message = $"Hey there! I'm a background processed message using Hangfire! This request was from the {step.WorkflowName} located on {step.RelativityInstanceURL}. Robots are cool! Beep boop beep :robot_face:";
 
             try
             {
@@ -37,9 +38,9 @@ namespace DelegateLearningDocs.Controllers
 
         [HttpPost]
         [Route("slackmessage")]
-        public async Task<IActionResult> SlackMessageWebhook()
+        public async Task<IActionResult> SlackMessageWebhook(WebhookStep step)
         {
-            var message = "Hey there! I'm a basic Slack Endpoint webhook. I was sent for testing purposes! :pandadance:";
+            var message = $"Hey there! I'm a basic Slack Endpoint webhook. I was sent for testing purposes from {step.RelativityInstanceURL} in the {step.WorkflowName} workflow. :pandadance:";
             var contentObject = new { text = message };
             var contentJson = System.Text.Json.JsonSerializer.Serialize(contentObject);
             var content = new StringContent(contentJson, Encoding.UTF8, "application/json");
@@ -63,35 +64,35 @@ namespace DelegateLearningDocs.Controllers
 
         [HttpPost]
         [Route("200")]
-        public IActionResult Delegate200Webhook()
+        public IActionResult Delegate200Webhook(WebhookStep step)
         {
-            return Ok("This is a 200 response.");
+            return Ok($"This is a 200 response. The request body values were: {step.WorkflowName}, {step.WorkspaceId}, {step.RelativityInstanceURL}, {step.InstanceId}");
         }
 
         [HttpPost]
         [Route("400")]
-        public IActionResult Delegate400Webhook()
+        public IActionResult Delegate400Webhook(WebhookStep step)
         {
             return BadRequest("This is a 400 response.");
         }
 
         [HttpPost]
         [Route("404")]
-        public IActionResult Delegate404Webhook()
+        public IActionResult Delegate404Webhook(WebhookStep step)
         {
             return NotFound("This is a 404 response.");
         }
 
         [HttpPost]
         [Route("500")]
-        public IActionResult Delegate500Webhook()
+        public IActionResult Delegate500Webhook(WebhookStep step)
         {
             return StatusCode(500, "This is a 500 response.");
         }
 
         [HttpPost]
         [Route("timeout")]
-        public IActionResult DelegateTimeoutWebhook()
+        public IActionResult DelegateTimeoutWebhook(WebhookStep step)
         {
             Thread.Sleep(60000);
             return Ok("I have finished sleeping for 60 seconds!");
---
+++ src/API/Models/WebhookStep.cs
@@ -0,0 +1,10 @@
+﻿namespace DelegateLearningDocs.Models
+{
+    public class WebhookStep
+    {
+        public string? InstanceId { get; set; }
+        public string? WorkspaceId { get; set; }
+        public string? WorkflowName { get; set; }
+        public string? RelativityInstanceURL { get; set; }
+    }
+}
