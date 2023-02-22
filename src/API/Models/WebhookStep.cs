8ab3344f58d027dea3aebda82f30e38a5d70f146
Tim Randall <Tim@milyli.com>
2023-02-22 16:20:36 -0600

    chore: Change WorkspaceId type to int for the WebhookStep model.

--- src/API/Models/WebhookStep.cs
+++ src/API/Models/WebhookStep.cs
@@ -4,7 +4,7 @@
     {
         public string? InstanceId { get; set; }
         public string? FriendlyInstanceName { get; set; }
-        public string? WorkspaceId { get; set; }
+        public int? WorkspaceId { get; set; }
         public string? WorkflowName { get; set; }
         public string? RelativityInstanceURL { get; set; }
     }
